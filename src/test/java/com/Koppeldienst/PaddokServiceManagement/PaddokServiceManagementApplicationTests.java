715b30bdc20065b7f1f2dd980d18a50cc1b7bd8c
Jona <jona.bock20@gmail.com>
2025-04-14 20:03:12 +0200

    first commit

---
+++ src/test/java/com/Koppeldienst/PaddokServiceManagement/PaddokServiceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Koppeldienst.PaddokServiceManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaddokServiceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
